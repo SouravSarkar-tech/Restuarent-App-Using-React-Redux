1d62b7171586789ad30c2a6130372860b5b06e51
Sourav Sarkar <souravsarkar0022@gmail.com>
2022-06-16 21:05:30 +0530

    Error Page Added

--- reduxapp/src/All-Pages/Error-Page/Error.js
+++ reduxapp/src/All-Pages/Error-Page/Error.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+import './Error.css'
 
 const Error = () => {
+    const navigate = useNavigate()
   return (
+    <div className='cover'>
+       <h1>Hmmm! That doesn't looks good!</h1>
     <div>
-      Error
+        <p>Go back to <a href="" onClick={() => navigate('/')}>Home Page</a></p>
+    </div>
+
     </div>
   )
 }
